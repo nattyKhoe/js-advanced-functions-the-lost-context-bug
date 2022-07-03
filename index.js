2c66bc84a8cf78aee85dcc44cc79529219217301
nattyKhoe <natasha.kusuma@outlook.com>
2022-07-03 17:54:14 +1000

    Done

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// let configuration = {
+//   frontContent: "Happy Birthday, Odin One-Eye!",
+//   insideContent: "From Asgard to Nifelheim, you're the best all-father ever.\n\nLove,",
+//   closing: {
+//       "Thor": "Admiration, respect, and love",
+//       "Loki": "Your son"
+//   },
+//   signatories: [
+//       "Thor",
+//       "Loki"
+//   ]
+// }
+
+// let printCard = function() {
+//   console.log(this.frontContent)
+//   console.log(this.insideContent)
+//   // Wow! Elegant! And notice the arrow function's `this` is the same
+//   // this that printCard has by virtue of configuration being passed
+//   // in as a thisArg
+//   this.signatories.forEach(s => console.log(`${this.closing[s]}, ${s}`), this)
+// }
+
+// printCard.call(configuration)
+
 let configuration = {
   frontContent: "Happy Birthday, Odin One-Eye!",
   insideContent: "From Asgard to Nifelheim, you're the best all-father ever.\n\nLove,",
@@ -9,16 +33,19 @@ let configuration = {
       "Thor",
       "Loki"
   ]
-}
+};
 
 let printCard = function() {
   console.log(this.frontContent)
   console.log(this.insideContent)
-  // Wow! Elegant! And notice the arrow function's `this` is the same
-  // this that printCard has by virtue of configuration being passed
-  // in as a thisArg
-  this.signatories.forEach(s => console.log(`${this.closing[s]}, ${s}`)
-  )
+
+  console.log("Debug Before forEach: " + this)
+  this.signatories.forEach(function(signatory){
+      let message = `${this.closing[signatory]}, ${signatory}`
+      console.log(message)
+  }, this)
 }
 
 printCard.call(configuration)
+
+
